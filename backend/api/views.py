604bbcf5075dbeb8a41a990e2ec042e105500fec
Mansi Jaiswal <mini.mansi22@gmail.com>
2023-09-16 01:24:21 +0530

    Added contact api

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,11 +1,32 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
+from rest_framework.parsers import JSONParser
+from api.serializers import CustomerSerializer
+from api.models import Customers
+from django.views.decorators.csrf import csrf_exempt
+
+
 # Create your views here.
+@csrf_exempt
 def index(request):
     return HttpResponse("Hello from index")
 
+@csrf_exempt
 def home(request):
     return HttpResponse("Hello from home")
 
+@csrf_exempt
 def contact(request):
+    if request.method == 'POST':
+        data = JSONParser().parse(request)
+        serializer = CustomerSerializer(data=data)
+        if serializer.is_valid():
+            serializer.save()
+            return JsonResponse(serializer.data, status=201)
+        return JsonResponse(serializer.errors, status=400)
+    elif request.method == 'GET':
+        customers = Customers.objects.all()
+        serializer = CustomerSerializer(customers, many=True)
+        return JsonResponse(serializer.data, safe=False)
     return HttpResponse("Hello from contact")
+
