a851231ef75664d4fbfbf88e26e4baddbfc5ba81
Mansi Jaiswal <mini.mansi22@gmail.com>
2023-09-16 01:58:21 +0530

    Using Postgres

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        "ENGINE": "django.db.backends.postgresql_psycopg2",
+        "NAME": "mydb",
+        "USER": "mansi22",
+        "PASSWORD": "",
+        "HOST": "localhost",
+        "PORT": "5431",
     }
 }
 
